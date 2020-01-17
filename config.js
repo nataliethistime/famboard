bc1e20f78da497ae78ea637869f5840447be4c74
Nathan McCallum <nathan@1vas.me>
2020-01-17 17:31:50 +1000

    Implement a little config module

---
+++ config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const _ = require('lodash');
+
+const config = {
+  isProduction() {
+    return process.env.NODE_ENV === 'production';
+  },
+
+  get(key) {
+    return process.env['FAMBOARD_' + String(key).toUpperCase()] || defaults[key] || undefined;
+  },
+
+  toObject() {
+    return _.mapValues(defaults, (val, key) => config.get(key));
+  }
+};
+
+const defaults = {
+  name: config.isProduction() ? 'Untitled' : 'Developer Board',
+  password: '1234qwer',
+};
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const express = require('express');
+const config = require('./config');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello, World!'));
 
-app.listen(port, () => console.log('Listening at localhost:' + port));
+app.listen(port, () => {
+  console.log('Listening at localhost:' + port)
+  console.log(config.toObject());
+});
